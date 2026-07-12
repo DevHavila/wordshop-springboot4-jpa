683cd922a9b50b455c512677bfe7418e4089bc3a
Hávila Júnior <havilajunior005@gmail.com>
2026-07-12 11:42:42 -0400

    Service layer, component registration

---
+++ src/main/java/com/educandoweb/corse/services/UserService.java
@@ -0,0 +1,26 @@
+package com.educandoweb.corse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.corse.entities.User;
+import com.educandoweb.corse.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
